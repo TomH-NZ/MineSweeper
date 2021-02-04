01d1b97bfa4e4f7f92e908cbb74a42ab684a2417
Tom Hatcher <tom.hatcher@myob.com>
2021-02-05 10:39:54 +1300

    Added first tests for GameOver message. Added classes for player lost/won status.

--- MineSweeperUnitTests/MineSweeperUnitTestsShould.cs
+++ MineSweeperUnitTests/MineSweeperUnitTestsShould.cs
@@ -1,4 +1,5 @@
 using System;
+using MineSweeper_v01.GameMessages;
 using Xunit;
 
 namespace MineSweeperUnitTests
@@ -9,10 +10,26 @@ namespace MineSweeperUnitTests
         public void ReturnGameOverMessageWhenPlayerLoses()
         {
             //Arrange
-            
+            var gameOver = new PlayerLost();
+
             //Act
-            
+            var result = gameOver.GameOverMessage(true);
+
             //Assert
+            Assert.Equal("Game Over, man.  Game Over.", result);
+        }
+
+        [Fact]
+        public void ReturnGameOverMessageWhenPlayerWins()
+        {
+            //Arrange
+            var gameOver = new PlayerWins();
+
+            //Act
+            var result = gameOver.GameOverMessage(true);
+
+            //Assert
+            Assert.Equal("You've cleared the minefield and won!", result);
         }
     }
 }
