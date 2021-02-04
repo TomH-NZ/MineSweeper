32cd26208e4e9583a4a9c4e09d1f792d75531dea
Tom Hatcher <tom.hatcher@myob.com>
2021-02-05 10:24:49 +1300

    Added additional folders, added unit tests.

---
+++ MineSweeperUnitTests/MineSweeperUnitTestsShould.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace MineSweeperUnitTests
+{
+    public class MineSweeperUnitTestsShould
+    {
+        [Fact]
+        public void ReturnGameOverMessageWhenPlayerLoses()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+        }
+    }
+}
---
+++ MineSweeper_v01/GameMessages/PlayerLost.cs
@@ -0,0 +1,10 @@
+namespace MineSweeper_v01.GameMessages
+{
+    public class PlayerLost
+    {
+        public string GameOverMessage(bool playerStatus)
+        {
+            return "Game Over, man.  Game Over.";
+        }
+    }
+}
