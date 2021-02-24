e3e90f91bd199b185cecb5b9b151ab56430d82e1
Tom Hatcher <tom.hatcher@myob.com>
2021-02-25 10:36:49 +1300

    Refactored DisplayGrid to clean up unused code.

--- MineSweeper/GridClass/DisplayGrid.cs
+++ MineSweeper/GridClass/DisplayGrid.cs
@@ -17,9 +17,15 @@ namespace MineSweeper_v01
         public string GenerateGameDisplay(int size)
         {
             var gameGrid = Factory.NewGameGrid(size);
+            
             var output = gameGrid.GeneratedGameCell.Cast<Cell>().Aggregate("", (current, variable) => current + ". ");
 
-            /*for (var row = 0; row < size; row++)
+            return output;
+        }
+    }
+}
+
+/*for (var row = 0; row < size; row++)
             {
                 for (var column = 0; column < size; column++)
                 {
@@ -29,23 +35,3 @@ namespace MineSweeper_v01
 
                 output += "\n";
             }*/
-
-            return output;
-        }
-    }
-}
-/*foreach (var mine in mineGenerator.MineLocations(size)) // ToDo: Look at extracting this to a separate class.
-{
-    var cellCoordinates = row + "," + column;
-
-    if (mine == cellCoordinates) // ToDo: look into a contains method, or a LINQ query
-    {
-        GeneratedGameCell[row, column] = new Cell(row, column, CellStatus.OccupiedByMine);
-    }
-    else
-    {
-        GeneratedGameCell[row, column] = new Cell(row, column, CellStatus.NotOccupiedByMine); 
-    }
-}
-            var mineGenerator = Factory.NewMineLocations();
-            */
