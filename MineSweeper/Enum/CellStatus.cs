f1c43f0e99adfb163212c2103d26b22addb14903
Tom Hatcher <tom.hatcher@myob.com>
2021-03-02 09:29:51 +1300

    Deleted unused enum file and directory.

--- MineSweeper/Enum/CellStatus.cs
+++
@@ -1,8 +0,0 @@
-// ReSharper disable once CheckNamespace
-namespace MineSweeper_v01
-{
-    public enum CellStatus // ToDo: Delete this class / directory if unused.
-    {
-        
-    }
-}
--- MineSweeper/GridClass/Cell.cs
+++ MineSweeper/GridClass/Cell.cs
@@ -2,7 +2,7 @@
 // ReSharper disable once CheckNamespace
 namespace MineSweeper_v01
 {
-    public class Cell // TODO: DELETE ENUM
+    public class Cell
     {
         public int RowLocationValue { get; }
         public int ColumnLocationValue { get; }
