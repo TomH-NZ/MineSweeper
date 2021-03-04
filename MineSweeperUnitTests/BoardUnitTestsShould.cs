150b561c1b8b089bfa5cc5afe41cab775922ceb4
Tom Hatcher <tom.hatcher@myob.com>
2021-03-04 13:12:17 +1300

    Split mine logic tests into new file.

--- MineSweeperUnitTests/BoardUnitTestsShould.cs
+++ MineSweeperUnitTests/BoardUnitTestsShould.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using MineSweeper_v01;
 using Xunit;
 
@@ -37,49 +36,6 @@ namespace MineSweeperUnitTests
             
             //Assert
             Assert.Equal(expected, result);
-        }
-
-        private class StubForMineLocationZeroZero : IMineGenerator
-        {
-            public List<Cell> MineLocations(int gridSize)
-            {
-                var output = new List<Cell> {new Cell(0,0)};
-                return output;
-            }
-        }
-        
-        [Fact]
-        public void UpdateTheStatusOfACellToRecordAMineAsTrue()
-        {
-            //Arrange
-            var gridSize = 2;
-            var newGameGrid = GridFactory.NewGameGrid(gridSize);
-            newGameGrid.GenerateGrid(newGameGrid.Size);
-            var updateCellMineStatus = new MineLogic();
-            var mineStub = new StubForMineLocationZeroZero();
-
-            //Act
-            updateCellMineStatus.UpdateCellMineStatus(mineStub.MineLocations(newGameGrid.Size), newGameGrid);
-
-            //Assert
-            Assert.True(newGameGrid.GeneratedGameCell[0,0].IsAMine);
-        }
-        
-        [Fact]
-        public void UpdateTheStatusOfACellToRecordAMineAsFalse()
-        {
-            //Arrange
-            var gridSize = 2;
-            var newGame = GridFactory.NewGameGrid(gridSize);
-            newGame.GenerateGrid(newGame.Size);
-            var updateCellMineStatus = new MineLogic();
-            var mineStub = new StubForMineLocationZeroZero();
-
-            //Act
-            updateCellMineStatus.UpdateCellMineStatus(mineStub.MineLocations(newGame.Size), newGame);
-
-            //Assert
-            Assert.False(newGame.GeneratedGameCell[1,1].IsAMine);
         }
     }
 }
